4c4552d67ed439a96fbabbdf0c40a598a6e104fb
Ahmed <ahmnouira@gmail.com>
2022-06-02 22:16:58 +0100

    models

--- moviereviews/movies/models.py
+++ moviereviews/movies/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Movie(models.Model): 
+    # string field for small to large sized strings
+    # max_length argument is required
     title: models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image  = models.ImageField(upload_to="movies/images")
+
+    # blank: mean that this field is optional
     url = models.URLField(blank=True)
