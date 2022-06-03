da2f23a24f115fc0ab9b279fe189f30abb40ac42
Ahmed <ahmnouira@gmail.com>
2022-06-03 01:07:32 +0100

    added news app

--- moviereviews/moviereviews/urls.py
+++ moviereviews/moviereviews/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -28,7 +28,10 @@ urlpatterns = [
     # "about" Page not found (404)
     path("about/", movieViews.about, name='about'), 
     path("contact", movieViews.contact, name="contact"),
-    path("signup", movieViews.signup, name="signup")
+    path("signup", movieViews.signup, name="signup"), 
+    
+    # will forward any requests with "news" to news apps urls.py
+    path("news/", include("news.urls"))
 ]
 
 
---
+++ moviereviews/news/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+from .models import News
+
+# Create your views here.
+def news(request:HttpRequest): 
+    # most recent news 
+    data  =  News.objects.all().order_by('-date')
+    return render(request, "news.html", {'data': data})
+
