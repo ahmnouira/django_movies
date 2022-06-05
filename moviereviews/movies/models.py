089f365d7492b4c3cd8e6c1438a0747648a93977
Ahmed <ahmnouira@gmail.com>
2022-06-06 00:01:50 +0100

    added review model

--- moviereviews/movies/models.py
+++ moviereviews/movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Movie(models.Model): 
@@ -13,3 +14,20 @@ class Movie(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class Review(models.Model): 
+
+    text  = models.CharField(max_length=100)
+    watch_agin = models.BooleanField()
+    # when someone creates  this object, the current datetime will be automatically filled in 
+    # note this makes the field non-editable, once the datetime is set, it is fixed
+    date  = models.DateTimeField(auto_now_add=True)
+    # a user can create multiple reviews, a movie similarly **can have** similarly review
+    # for user, the reference is to build-in user model that Django provides for authentication 
+    user  = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie  = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.text
+
