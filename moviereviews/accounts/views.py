71b53eeb278350fd4f5946854f3fc126175c874e
Ahmed <ahmnouira@gmail.com>
2022-06-05 04:42:36 +0100

    added signup form

--- moviereviews/accounts/views.py
+++ moviereviews/accounts/views.py
@@ -1,13 +1,33 @@
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout
 
 # Create your views here.
 
-
 def signup(request: HttpRequest): 
 
-    return render(request, 'signup.html', {
-        'form': UserCreationForm
-    })
+    if request.method == "GET": 
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else: 
+        password1 = request.POST["password1"] 
+        password2 = request.POST["password2"]
+        if password1 == password2: 
+            username  = request.POST['username']
+            user  = User.objects.create_user(username, password1)
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else: 
+            return render(request, 'signup.html', {
+                'form': UserCreationForm, 'error': 'Passwords do not match'
+            })
+
+
+
+
+
 
